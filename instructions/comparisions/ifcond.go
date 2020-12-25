8a2dc84d6e4e837af59e7f33e518237eb2ceddd8
Zesheng Zong <zong.ze@husky.neu.edu>
2020-12-25 23:10:47 +0800

    add instructions

---
+++ instructions/comparisions/ifcond.go
@@ -0,0 +1,20 @@
+package comparisions
+
+import (
+	"gvm/instructions/base"
+	"gvm/rtda"
+)
+
+type IFEQ struct{ base.BranchInstruction }
+type IFNE struct{ base.BranchInstruction }
+type IFLT struct{ base.BranchInstruction }
+type IFLE struct{ base.BranchInstruction }
+type IFGT struct{ base.BranchInstruction }
+type IFGE struct{ base.BranchInstruction }
+
+func (self *IFEQ) Execute(frame *rtda.Frame) {
+	val := frame.OperandStack().PopInt()
+	if val == 0 {
+		base.Branch(frame, self.Offset)
+	}
+}
