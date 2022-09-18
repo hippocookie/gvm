bdcd73dda80372e4fa13cb092ccafa1ffac160f5
hippocookie <zesheng.zong@outlook.com>
2022-09-18 23:34:20 +0800

    Add class file

--- classfile/class_file.go
+++ classfile/class_file.go
@@ -16,3 +16,65 @@ type ClassFile struct {
 	attributes []AttributeInfo
 }
 
+func Parse(classData []byte) (cf *ClassFile, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			var ok bool
+			err, ok = r.(error)
+			if !ok {
+				err = fmt.Errorf("%v", r)
+			}
+		}
+	}()
+	cr := &ClassReader{classData}
+	cf = *ClassFile{}
+	cf.read(cr)
+}
+
+func (self *ClassFile) read(reader *ClassReader) {
+
+}
+
+func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
+
+}
+
+func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
+
+}
+
+func (self *ClassFile) MinorVersion() uint16 {
+
+}
+
+func (self *ClassFile) MajorVersion() uint16 {
+
+}
+
+func (self *ClassFile) ConstantPool() ConstantPool {
+
+}
+
+func (self *ClassFile) AccessFlags() uint16 {
+
+}
+
+func (self *ClassFile) Fields() []*MemberInfo {
+
+}
+
+func (self *ClassFile) Methods() []*MemberInfo {
+
+}
+
+func (self *ClassFile) ClassName() string {
+
+}
+
+func (self *ClassFile) SuperClassName() string {
+
+}
+
+func (self *ClassFile) InterfaceNames() []string {
+
+}
