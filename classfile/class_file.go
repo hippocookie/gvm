35d4bd3df1d5313125a23b250293c27d762e2e83
hippocookie <zesheng.zong@outlook.com>
2022-09-20 23:52:16 +0800

    Add member info

--- classfile/class_file.go
+++ classfile/class_file.go
@@ -46,11 +46,24 @@ func (self *ClassFile) read(reader *ClassReader) {
 }
 
 func (self *ClassFile) readAndCheckMagic(reader *ClassReader) {
-
+	magic := reader.readUint32()
+	if magic != 0xCAFEBABE {
+		panic("java.lang.ClassFormatError: magic")
+	}
 }
 
 func (self *ClassFile) readAndCheckVersion(reader *ClassReader) {
-
+	self.minorVersion = reader.readUint16()
+	self.majorVersion = reader.readUint16()
+	switch self.majorVersion {
+		case 45:
+		return
+		case 46, 47, 48, 49, 50, 51, 52:
+		if self.minorVersion == 0 {
+			return
+		}
+	}
+	panic("java.lang.UnsupportedClassVersionError!")
 }
 
 func (self *ClassFile) MinorVersion() uint16 {
